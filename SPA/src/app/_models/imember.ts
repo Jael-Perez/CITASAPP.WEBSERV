4a80f3c25bc9884d8266e7966810908a055b21b0
Jael-Perez <perez.jaelalejandro@gmail.com>
2023-11-15 08:56:04 -0600

    Added ng-gallery

---
+++ SPA/src/app/_models/imember.ts
@@ -0,0 +1,22 @@
+﻿
+import {IPhoto} from "./iphoto";
+
+export interface IMember {
+
+    id: number;
+    userName: string;
+    photoUrl: string; 
+    age: number; 
+    knownAs: string;
+    created: Date; 
+    lastActive: Date; 
+    gender: string; 
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: IPhoto[];
+}
+
+    
