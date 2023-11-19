2d24fbd450d782bbe0346557d9f62fae251c423d
Jael-Perez <perez.jaelalejandro@gmail.com>
2023-11-19 13:48:34 -0600

    Commit para reparar errores

--- SPA/src/app/_interceptors/jwt.interceptor.ts
+++ SPA/src/app/_interceptors/jwt.interceptor.ts
@@ -6,24 +6,26 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable, take } from 'rxjs';
+import { AccountService } from '../_services/account.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user =>{
-        if(user){
-          request =request.clone({
+      next: user => {
+        if (user) {
+          request = request.clone({
             setHeaders: {
               Authorization: `Bearer ${user.token}`
             }
           });
         }
       }
-    })
+    });
+    
     return next.handle(request);
   }
 }
