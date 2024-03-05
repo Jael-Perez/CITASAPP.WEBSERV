fe1c1e0e0a54c3eb36fded510a0873b6b6597ce4
Jael-Perez <perez.jaelalejandro@gmail.com>
2024-03-04 21:07:01 -0600

    Added the main photo thumbnail to nav bar

--- SPA/src/app/_services/account.service.ts
+++ SPA/src/app/_services/account.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { IUser } from '../_models/iuser';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = "https://localhost:5001/api/";
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<IUser | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
@@ -18,8 +19,7 @@ export class AccountService {
       map((response: IUser) => {
         const user = response;
         if (user) {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -29,14 +29,14 @@ export class AccountService {
     return this.http.post<IUser>(this.baseUrl + "account/register", model).pipe(
       map(user => {
         if (user) {
-          localStorage.setItem("user", JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
   }
 
   setCurrentUser(user: IUser) {
+    localStorage.setItem("user", JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
